b0ed82dc06e25e6b5def2f0e04b5fbf6b87a7466
vaibhav singh <vaibhav.anmol.chandel@gmail.com>
2024-01-04 17:59:43 +0530

    Done

--- src/component/GptMovieSuggesion.js
+++ src/component/GptMovieSuggesion.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { IMG_CDN_URL } from '../utils/constants'
 
 const GptMovieSuggesion = () => {
     const movieRsults = useSelector(store => store.gpt)
+    // const [filterM, setFilterM] = useState('')
     // const lastOne = movieRsults.gptMovies
     // console.log(lastOne)
     // console.log(movieRsults.gptMovies[0].overview)
+    console.log(movieRsults?.gptMovies)
 
-    console.log(movieRsults)
+    const filterResult = movieRsults?.gptMovies?.filter((movie) => movie.poster_path != null)
+    // setFilterM(filterResult)
+    console.log(filterResult)
+    // console.log(filterM)
+
+
     return (
         <div className=' text-black flex gap-8 sm:gap-16 my-20 flex-wrap'>
-            {movieRsults?.gptMovies?.map((movie) => (
+            {filterResult.map((movie) => (
 
-                <div className=" border-black shadow shadow-black rounded bg-gray-200 p-3 box my-0 mx-auto" key={movie.id}>
+
+                <div className=" border-black shadow shadow-black rounded bg-gray-200 p-2 sm:p-4 box my-0 mx-auto" key={movie.id}>
                     <div className=" h-1/3"><img className=' w-full h-full rounded' src={IMG_CDN_URL + movie.poster_path} alt="Img" /></div>
                     <h2 className=' font-semibold text-lg mt-7'>{movie.original_title}</h2>
                     <h2>{movie.original_language}</h2>
                     <h2>{movie.release_date}</h2>
-                    {/* <h3>{movie.overview}</h3> */}
                 </div>
             ))}
-        </div>
+        </div >
     )
 }
 
